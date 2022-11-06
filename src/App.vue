d030f9d994f10b9547dfa42865d3bf7026487386
MrPerux <perschrijver@gmail.com>
2022-11-06 09:16:15 +0100

    Try out reactive data with on click changes.

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,9 @@
   <div class="app">
     Welcome to the wonderous world of Vue. Please look around. Stay a while.
   </div>
+  <span>My name is: {{ name }} </span>
+  <br />
+  <button @click="changeName('Johanathan')">Let's change that name</button>
 </template>
 
 <script lang="ts">
@@ -10,6 +13,14 @@ import { defineComponent } from "vue";
 export default defineComponent({
   name: "App",
   components: {},
+  data() {
+    return { name: "Sir Thomason III junior" };
+  },
+  methods: {
+    changeName(name: string) {
+      this.name = name;
+    },
+  },
 });
 </script>
 
